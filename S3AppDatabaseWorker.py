ea1900ec8010f201a64ee51ff55c34d46613e9cb
Monty Dimkpa <cmdimkpa@gmail.com>
2019-11-03 17:46:15 +0100

    Update S3AppDatabaseWorker.py

--- S3AppDatabaseWorker.py
+++ S3AppDatabaseWorker.py
@@ -7,6 +7,7 @@ from boto.s3.key import Key
 import requests as http
 from hashlib import md5
 from threading import Thread
+from Queue import Queue
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "S3AppDatabaseWorker"
@@ -50,14 +51,6 @@ def new_id():
     hasher = md5()
     hasher.update(now())
     return hasher.hexdigest()
-
-class Queue:
-    def __init__(self):
-        self.queue = []
-    def put(self,item):
-        self.queue.insert(0,item)
-    def get(self):
-        return self.queue.pop()
 
 def event_handler(event):
     try:
