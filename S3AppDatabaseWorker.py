5d7f18e1130f7b7bc53316ccbc13542d79839cb1
Monty Dimkpa <cmdimkpa@gmail.com>
2019-11-03 10:21:03 +0100

    Update S3AppDatabaseWorker.py

--- S3AppDatabaseWorker.py
+++ S3AppDatabaseWorker.py
@@ -89,31 +89,28 @@ def RunParallelS3Events(Events,runtime_key):
         t.start()
 
 def AsyncS3MessagePolling(Events):
-    global MESSAGE_BUS, current_polls, poll_delay_secs
-    def scan():
-        global current_polls
-        sleep(poll_delay_secs)
-        current_polls+=1
-    runtime_key = new_id(); RunParallelS3Events(Events,runtime_key); message = null
-    max_poll_secs = 3; poll_delay_secs = 0.3; max_polls = max_poll_secs // poll_delay_secs; current_polls = 0
-    while not message and current_polls < max_polls:
+    global MESSAGE_BUS, Message, runtime_key
+    def completed():
+        global MESSAGE_BUS, Message
         result_index = [MESSAGE_BUS.index(entry) for entry in MESSAGE_BUS if runtime_key in entry]
-        if result_index:
+        if not result_index:
+            return false
+        else:
             entry = MESSAGE_BUS[result_index[0]]
             if len(entry[runtime_key]) == len(Events):
-                MESSAGE_BUS.pop(result_index[0])
-                message = entry[runtime_key]
+                message = MESSAGE_BUS.pop(result_index[0])
+                result = [data for data in message if data]
+                if result:
+                    Message = {entry.keys()[0]:entry[entry.keys()[0]] for entry in result}
+                else:
+                    Message = null
+                return true
             else:
-                scan()
-        else:
-            scan()
-    if message:
-        result = [data for data in message if data]
-        if result:
-            message = {entry.keys()[0]:entry[entry.keys()[0]] for entry in result}
-        else:
-            message = null
-    return message
+                return false
+    runtime_key = new_id(); Message = null; RunParallelS3Events(Events,runtime_key)
+    while not completed():
+        sleep(0)
+    return Message
 
 def responsify(status,message,data={}):
     code = int(status)
